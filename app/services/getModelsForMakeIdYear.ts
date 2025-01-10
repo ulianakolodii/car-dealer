e474a37c452681dfddb2ae1159ab80f0a2bab98f
Uliana Kolodii <ulianakolodii@gmail.com>
2025-01-10 14:14:09 +0200

    feat: add error handling to model retrieval function and return empty array on failure

--- app/services/getModelsForMakeIdYear.ts
+++ app/services/getModelsForMakeIdYear.ts
@@ -1,13 +1,18 @@
 import { Model, ResultPageParams } from '../types';
 
 const getModelsForMakeIdYear = async ({ makeId, year }: ResultPageParams) => {
-  const response = await fetch(
-    `${process.env.API_URL}api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
-  );
-  const data: {
-    Results?: Array<Model>;
-  } = await response.json();
-  return data.Results;
+  try {
+    const response = await fetch(
+      `${process.env.API_URL}api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
+    );
+    const data: {
+      Results?: Array<Model>;
+    } = await response.json();
+    return data.Results;
+  } catch (error) {
+    console.error('Error fetching models for make ID and year:', error);
+    return [];
+  }
 };
 
 export default getModelsForMakeIdYear;
