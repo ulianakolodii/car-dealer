cd26ae5e5e296f146cddbd786ee297c71ea76fc7
Uliana Kolodii <ulianakolodii@gmail.com>
2025-01-10 12:39:24 +0200

    feat: improve vehicle model retrieval with error handling and notFound redirection

--- app/result/[makeId]/[year]/page.tsx
+++ app/result/[makeId]/[year]/page.tsx
@@ -4,11 +4,11 @@ import { ResultPageParams } from "@/app/types";
 import generateYearsRange from "@/app/utils/generateYearsRange";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { notFound } from "next/navigation";
 
 export async function generateStaticParams() {
   const makes = await getMakesForVehicleType();
@@ -33,38 +33,29 @@ const ResultPage = async ({
 }) => {
   const { makeId, year } = await params;
 
-  try {
-    const vehicleData = await getModelsForMakeIdYear({ makeId, year });
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6">
-          Vehicle Models for Make ID {makeId} and Year {year}
-        </h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {vehicleData.map((model) => (
-            <Card key={model.Model_ID}>
-              <CardHeader>
-                <CardTitle>{model.Model_Name}</CardTitle>
-                <CardDescription>Make: {model.Make_Name}</CardDescription>
-              </CardHeader>
-              <CardContent>Model ID: {model.Model_ID}</CardContent>
-            </Card>
-          ))}
-        </div>
-      </div>
-    );
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    console.error("Error fetching vehicle models", error);
-    return (
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6">Error</h1>
-        <p className="text-red-500">
-          Failed to fetch vehicle models. Please try again later.
-        </p>
-      </div>
-    );
+  const vehicleData = await getModelsForMakeIdYear({ makeId, year });
+  if (!vehicleData) {
+    notFound();
   }
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-6">
+        Vehicle Models for Make ID {makeId} and Year {year}
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {vehicleData.map((model) => (
+          <Card key={model.Model_ID}>
+            <CardHeader>
+              <CardTitle>{model.Model_Name}</CardTitle>
+              <CardDescription>
+                {model.Make_Name} - # {model.Model_ID}
+              </CardDescription>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ResultPage;
--- app/services/getModelsForMakeIdYear.ts
+++ app/services/getModelsForMakeIdYear.ts
@@ -5,7 +5,7 @@ const getModelsForMakeIdYear = async ({ makeId, year }: ResultPageParams) => {
     `${process.env.API_URL}api/vehicles/GetModelsForMakeIdYear/makeId/${makeId}/modelyear/${year}?format=json`
   );
   const data: {
-    Results: Array<Model>;
+    Results?: Array<Model>;
   } = await response.json();
   return data.Results;
 };
